820ff6f97bf06a036bbff877e816cb64fa264c13
Natalie Halawani <hala1224@gmail.com>
2019-03-12 22:08:27 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,25 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
-   get '/' do 
-     erb :index 
-   end 
+  get '/recipes/new' do
+    erb :new
+  end
+
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
    
    
 end
