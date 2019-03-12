58fe0e20777ffb62b9865596f7534863fc167ea3
Natalie Halawani <hala1224@gmail.com>
2019-03-12 22:03:09 +0000

    Automatically backed up by Learn

--- db/migrate/20190312215706_create_recipes.rb
+++ db/migrate/20190312215706_create_recipes.rb
@@ -2,7 +2,7 @@ class CreateRecipes < ActiveRecord::Migration
   def change
     create_table :recipes do |t| 
       t.string :name 
-      t.string :ingridients
-      t.decimal :time
+      t.string :ingredients
+      t.string :cook_time
   end
 end
